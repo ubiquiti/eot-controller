c9c5beeec8741a993321784abed4f10f4bf994c4
artjoms-faltins-ubnt <41567642+artjoms-faltins-ubnt@users.noreply.github.com>
2019-02-11 15:09:59 +0200

    1.5.0 (#8)

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ UEOT_HTTPS_PORT="20443"
 LOG_DIR="/var/log/unifi-led"
 LOG_FILENAME="led-controller.log"
 
-UEOT_VERSION="1.4.1"
+UEOT_VERSION="1.5.0"
 
 USERNAME="ueot"
 HOME_DIR="/home/${USERNAME}"
